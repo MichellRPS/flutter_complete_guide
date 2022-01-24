43c8600176eaf0e0c90825de608a10497d808b66
Michell Rezende de Paula e Silva <michell.rezende.p@gmail.com>
2022-01-24 16:48:23 -0300

    Update auth.dart

--- shop_app/lib/providers/auth.dart
+++ shop_app/lib/providers/auth.dart
@@ -26,7 +26,7 @@ class Auth with ChangeNotifier {
   Future<void> _authenticate(
       String email, String password, String urlSegment) async {
     final url = Uri.parse(
-        'https://identitytoolkit.googleapis.com/v1/accounts:$urlSegment?key=AIzaSyCsusZEVrsxgAtjufdGIlSDQ5cmqmgX9f8');
+        'https://identitytoolkit.googleapis.com/v1/accounts:$urlSegment?key=<API Key>');
 
     try {
       final response = await http.post(
